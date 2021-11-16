2016fbc01113c571c2aa6ca6913d597dcaa5d9e4
Jorge Adalberto Rodriguez Garcia <george_nlc@hotmail.com>
2021-11-16 08:56:27 -0600

    Add registrarCalificaciones function

--- functions/alumnosCalificaciones/calificaciones.js
+++ functions/alumnosCalificaciones/calificaciones.js
@@ -13,4 +13,26 @@ function registrarAlumnos (alumnos) {
     alert('Tu lista de alumnos esta completa');
 }
 
+const registrarCalificaciones = alumnosCalificaciones => {
+    let parciales = prompt('Ingrese el numero de parciales');
+    let numeroParciales = parseInt(parciales);
+
+    for (let i = 0; i < alumnos.length; i++) {
+        let sumaCalificaciones = 0;
+
+        for (let j = 0; j < numeroParciales; j++) {
+            let calificacion = prompt('Calificacion parcial: ' + j+1);
+            let numCalificacion = parseInt(calificacion);
+            sumaCalificaciones += numCalificacion;
+            
+            if (j === numeroParciales-1) 
+                alumnosCalificaciones.push(sumaCalificaciones / numeroParciales);
+
+        }
+    }
+    
+    alert('Las calificaciones estan listas');
+}
+
 registrarAlumnos(alumnos);
+registrarCalificaciones(alumnosCalificaciones);
